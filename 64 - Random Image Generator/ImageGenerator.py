fd40747e853ee660e354417c397261c8e9a89763
themario30 <themario80@gmail.com>
2018-11-05 01:11:31 -0600

    Made the code a bit more readable-ish

--- 64 - Random Image Generator/ImageGenerator.py
+++ 64 - Random Image Generator/ImageGenerator.py
@@ -1,22 +1,30 @@
+#my professor told me to write a comment on each
 from PIL import Image, ImageDraw
+#statement. I don't know why though.
 import random
 
 #In this 'challenge', we shall use python to create the image. It is easier to do, and it also will allow me to dust up on python since I have not used it since Luigi's Poker. That was a while ago.
 
-IMAGESIZE = 200
+#make the image random upon each successful run
+random.seed(None)
 
+#make the resolution of the image random cause why not!
+IMAGESIZE = random.randint(150, 300)
+
+#The Image File
 image = Image.new('RGB',(IMAGESIZE,IMAGESIZE))
+#The draw object that will actually draw on the image
 draw = ImageDraw.Draw(image)
 
-random.seed(None)
+#the color variable is a 3-tuple object which is randomized upon each iteration
+#had to crack up Wikipedia for this one
+#In mathematics, a tuple is a finite ordered list (sequence) of elements. - Wikipedia
+#In Laymans terms, it is a ordered list.
 
 for x in range(0, IMAGESIZE):
 	for y in range(0, IMAGESIZE):
-		#the color variable is a 3-tuple object which is randomized upon each iteration
-		#had to crack up Wikipedia for this one
-		#In mathematics, a tuple is a finite ordered list (sequence) of elements. - Wikipedia
-		#In Laymans terms, it is a ordered list.
 		color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 		draw.point((x,y),color)
 
+#finally, save the image
 image.save("random.png")
